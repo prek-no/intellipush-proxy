a23cea67c2ae04fadc83a8f9ff92437b34827349
ChrisRM <chris@hirvi.no>
2023-09-09 16:25:55 +0200

    Progress

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
 import {VercelRequest, VercelResponse} from "@vercel/node";
 import {allowCors} from "../lib/utils";
+import {createProxyMiddleware} from "http-proxy-middleware";
 
-const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const apiProxy = createProxyMiddleware({
     target: "https://api.intellipush.com",
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        "^/api": "", // strip "/api" from the URL
+    },
 });
 
 const handler = async (req: VercelRequest, res: VercelResponse) => {
